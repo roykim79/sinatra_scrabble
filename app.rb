2d80d7dd85f74b27ac5734077e8f3b16b97144b5
Roy Kim <rykim79@gmail.com>
2018-08-17 12:41:31 -0400

    add input/output views and app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
 also_reload 'lib/**/*.rb'
-require './lib/rectangle'
+require './lib/word'
+
+get '/' do
+  erb(:input)
+end
+
+get '/output' do
+  @word = params.fetch('word')
+  word = Word.new(@word)
+  @score = word.scrabble()
+  erb(:output)
+end
