44a9c9e954888acd8e8c473827149c42c4b9fb8e
Roy Kim <rykim79@gmail.com>
2018-08-17 12:01:03 -0400

    add tests for longer strings, uppercase, and numbers

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -7,5 +7,20 @@ describe Word do
       word = Word.new('a')
       expect(word.scrabble).to eq(1)
     end
+
+    it("returns a score of 5 for the word 'boo'") do
+      word = Word.new("boo")
+      expect(word.scrabble()).to eq(5)
+    end
+
+    it("returns a scrabble score for a word regardless of case") do
+      word = Word.new("oXen")
+      expect(word.scrabble()).to eq(11)
+    end
+
+    it("returns a scrabble score for a word while ignoring numbers") do
+      word = Word.new("1o2X4e5n")
+      expect(word.scrabble()).to eq(11)
+    end
   end
 end
